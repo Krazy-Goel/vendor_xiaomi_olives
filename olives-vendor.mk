fdae64745e7ee583dfab67ebd386f043650b4ff7
IAmDeadlylxrd <IAmDeadlylxrd@gmail.com>
2021-10-22 13:21:07 +0000

    olives: Drop stock acdb we use it from nokia

--- olives-vendor.mk
+++ olives-vendor.mk
@@ -6,7 +6,6 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/olives
 
 # Inherit fragments
-$(call inherit-product, vendor/xiaomi/olives/olives-vendor-acdb.mk)
 $(call inherit-product, vendor/xiaomi/olives/olives-vendor-camera.mk)
 $(call inherit-product, vendor/xiaomi/olives/olives-vendor-fingerprint.mk)
 $(call inherit-product, vendor/xiaomi/olives/olives-vendor-firmware.mk)
